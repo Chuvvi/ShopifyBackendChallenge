fd184f7b64328a704012b5c9ea92cfa6fea83c32
suhas <chuvvis@gmail.com>
2022-05-22 12:33:13 -0400

    Removed legacy code

--- app.js
+++ app.js
@@ -37,34 +37,3 @@ app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
-
-
-// const {inventory, warehouse} = require('./data');
-
-// async function main(){
-//   try{
-//     let params = {
-//       name: 'C',
-//       location: 'Jersey City',
-//       capacity: '250'
-//     }
-//     let p = {
-//       title: 'B',
-//       description: 'Something',
-//       cost: '100',
-//       stock: '1000',
-//       weight: '1'
-//     }
-//     // let res = await warehouse.create(params);
-//     // let res = await warehouse.returnBack('62882292c12cc4b58379738f','62882245d51fce1ab651ec19', '10');
-//     // let res = await inventory.create(p);
-//     // let res = await warehouse.addItem('62899100a380816cf243df29', '628990d5bc4993d3ee801122', '30');
-//     // let res = await inventory.remove('6289770cfaea56290200744d');
-//     console.log(res);
-//   }
-//   catch (e){
-//     console.log(e);
-//   }
-// }
-
-// main();
